cea45f98d192391fdda2a6a9cb91bab2bd19c48c
Сарсенбаев Нурбол <kuma_official@mail.ru>
2017-12-07 19:10:12 +0600

    login register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
+import { UsersService } from './shared/services/users.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { AppRoutingModule } from './app-routing.module';
     AuthModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    UsersService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
