67a2a3adaf153d4db32ada3901de4591cd2cb452
Сарсенбаев Нурбол <kuma_official@mail.ru>
2017-12-12 18:52:22 +0600

    fix system template

--- src/app/system/system.component.ts
+++ src/app/system/system.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'hm-system',
   templateUrl: './system.component.html',
-  styleUrls: ['./system.component.scss']
+  styleUrls: ['./system.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SystemComponent implements OnInit {
+
+  currentDate = new Date();
 
   constructor() { }
 
